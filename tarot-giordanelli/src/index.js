fe67eac3816478b5589f0c21d165dcd1fc2d631e
alexandre <alexandre.giordanelli@gmail.com>
2017-06-24 19:32:00 -0300

    Finalizo o desenvolvimento, faltando apenas a documentação. Criei o componente CardList e utilizei a biblioteca 'lodash/shuffle' que utiliza uma algoritmo muito bom de permutação aleatória. Utilizei também o 'react-flip-move' para animações. Utilizei js puro para fazer a requisão ajax ao arquivo tarot.json no repositório original do desafio. Compatibilidade com IE9+. Além dos testes que vinha fazendo, brinquei com a biblioteca de teste enzyme para testar a moda antiga. Muito boa por sinal!

--- tarot-giordanelli/src/index.js
+++ tarot-giordanelli/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<CardList />, document.getElementById('root'));
+ReactDOM.render(<CardList ajax='https://raw.githubusercontent.com/Personare/front-end-challenge/master/tarot.json' />, document.getElementById('root'));
 registerServiceWorker();
--- tarot-giordanelli/src/stories/index.js
+++ tarot-giordanelli/src/stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import CardDetail from '../CardDetail';
 import Card from '../Card';
+import CardList from '../CardList';
 
 var card = {
   "name": "O MAGO",
@@ -13,4 +14,5 @@ var url = "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/imag
 storiesOf('Tarot', module)
   .add('Tela de descrição da carta', () => <CardDetail {...card} url={url} />)
   .add('Carta', () => <Card {...card} url={url} />)
+  .add('Baralho', () => <CardList />);
   
