cb950e94629590426f61b0ee656e4c3852880705
Gabriel Almeida <g.almeida.azeredo@gmail.com>
2017-06-30 01:55:33 -0300

    Melhorando estilo da aplicação, criando um header parecido com o do site da personare, para dispertar algumas semelhanças rs. Também optei por remover a lib axios e utilizar um import direto para o tarot.json

--- tarot-challenge/src/tarot-list/TarotList.js
+++ tarot-challenge/src/tarot-list/TarotList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios/dist/axios.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './TarotList.css';
+import cards from './tarot.json'
 
 class TarotList extends Component {
     state = { cards: [] }
@@ -8,35 +9,28 @@ class TarotList extends Component {
     constructor(props) {
         super(props);
 
+        //Os métodos da classe não tem acesso ao this por padrão.. estranho hm
         this.getCards = this.getCards.bind(this);
     }
 
     getCards() {
-
-    	var self = this;
-
-        //obtém os cards com axios e adiciona ao state
-        axios.get('/tarot.json')
-            .then(function(response) {
-                self.setState(response.data);
-            })
-            .catch(function(error) {
-                //Toma alguma outra ação de erro.
-                console.log(error);
-            });
-
+    	this.setState(cards); 
     }
 
     componentDidMount() {
         this.getCards();
     }
 
+    shuffleCards() { }
+
+    hideCards() { }
+
     render() {
         
         return (
         	<div>
-        		{this.state.cards.map((card, index) => ( 
-	        		<div className="col-md-2">
+        		{this.state.cards.map((card, index) => (
+	        		<div className="col-md-2" key={index}>
 				        <div className="card">
 				        	<img className='card-img-top' src={this.state.imagesUrl + card.image} alt={card.name} />
 				        </div>
