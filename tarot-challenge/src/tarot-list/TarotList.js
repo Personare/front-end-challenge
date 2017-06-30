b9b6a53b3a8658f11dd7018c05fb96133d91ab01
Gabriel Almeida <g.almeida.azeredo@gmail.com>
2017-06-30 02:09:29 -0300

    Substituindo modal do bootstrap js por componente react-bootstrap Modal

--- tarot-challenge/src/tarot-list/TarotList.js
+++ tarot-challenge/src/tarot-list/TarotList.js
@@ -95,31 +95,24 @@ class TarotList extends Component {
                 
                 </Shuffle>
 
-                <div className="modal fade" id="cardModal" tabIndex="-1" role="dialog" aria-hidden="true">
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Carta -> Nome da Carta</h5>
-                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <div className="col-md-4">
-                                    <img className='card-img-top' src={cardTurned} alt=""/>
-                                </div>
-                                <div className="col-md-8">
-                                    <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices risus vel libero dignissim faucibus. Nam tortor felis, pulvinar ut magna ut, tincidunt mattis mauris. Sed dictum mi pretium est rutrum convallis. Sed non pretium ligula. Quisque eleifend leo ut tortor vehicula, eget consequat urna commodo. Mauris et posuere dui. Integer posuere lacus ut malesuada lobortis. Phasellus suscipit maximus purus nec semper. Quisque ut rhoncus tellus. Nullam et lacus arcu. Nulla ante elit, iaculis at metus at, cursus congue velit. Fusce at turpis lectus. Pellentesque eget efficitur lorem, et egestas tortor. Pellentesque sed ornare lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Suspendisse auctor varius aliquet. Donec at libero a velit bibendum tempor ut sed diam. Maecenas et placerat nibh. Fusce luctus metus lectus, ac facilisis tortor imperdiet id. Nullam tristique dictum nibh a tristique. Pellentesque ornare molestie mattis. Nulla eget semper dui, convallis egestas massa. Pellentesque eleifend porttitor augue in scelerisque. Integer sollicitudin, sapien venenatis tempus varius, metus arcu molestie nibh, in commodo eros enim a erat. Fusce sed sagittis leo, non sollicitudin diam. Sed sodales massa vitae augue volutpat, sit amet finibus lorem pretium.</h2>
-                                    <p></p>
-                                </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={this.shuffleCards}>Fechar</button>
-                            </div>
+                <Modal show={this.state.showModal} onHide={this.close}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Carta -> xxxx</Modal.Title>
+                    </Modal.Header>
+                    
+                    <Modal.Body>
+                        <div className="col-md-4">
+                            <img className='card-img-top' src={this.state.imagesUrl + card.selectedCard.image} alt={card.selectedCard.name} />
                         </div>
-                    </div>
-                </div>
+                        <div className="col-md-8">
+                            <h4>Descrição da carta xxxx</h4>
+                        </div>
+                    </Modal.Body>
+                    
+                    <Modal.Footer>
+                        <Button onClick={this.close}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
         	</div>
         );
     }
