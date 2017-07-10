da55fec56049066bbe2baab00e8b1aa44ce9094e
matheus <matheusslima7@gmail.com>
2017-07-10 15:37:33 -0300

    guardando em um json hard code a informacao que deve ser apresentada no rnder, acessando a informacao do json

--- jogotarot/src/App.js
+++ jogotarot/src/App.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {cartas:[ 
+      {image: 'https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano1.jpg'}
+      ]};
+  }
+
   render() {
     return (
-      <div className="card">
-        <div className="face side1">
-          <img src="http://www.adrianparr.com/images/card_back.png" />
-        </div>
-        <div className="face side2">
-            <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano1.jpg" />
-        </div>  
-      </div>  
+      <section id="wrapper">
+          <ul>
+            {
+              this.state.cartas.map(function(carta, i){
+                return(
+                    <div className="card" key="i">{carta.image}
+                      <div className="face side1">
+                        <img src="http://www.adrianparr.com/images/card_back.png" />
+                      </div>
+                      <div className="face side2">
+                          <img src="{carta.image}" />
+                      </div>  
+                    </div>    
+                  );
+              })
+            }
+          </ul>
+      </section>
     );
   }
 }
