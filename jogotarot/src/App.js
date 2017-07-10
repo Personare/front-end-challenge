72a3703c9c83dbf29545f2373163c8e0a90d8618
matheus <matheusslima7@gmail.com>
2017-07-10 15:12:43 -0300

    colocando json tarot em public para ser acessivel, criando html e css das cartas

--- jogotarot/src/App.js
+++ jogotarot/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="card">
+        <div className="face side1">
+          <img src="http://www.adrianparr.com/images/card_back.png" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="face side2">
+            <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano1.jpg" />
+        </div>  
+      </div>  
     );
   }
 }
