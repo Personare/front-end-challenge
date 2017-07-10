f3b8474640334a233250baeeac5726088c0db4d1
matheus <matheusslima7@gmail.com>
2017-07-10 16:20:45 -0300

    pegando cartas dinamicamente do json com fetch

--- jogotarot/src/App.js
+++ jogotarot/src/App.js
@@ -5,9 +5,15 @@ class App extends Component {
 
   constructor(){
     super();
-    this.state = {cartas:[ 
-      {image: 'https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano1.jpg'}
-      ]};
+    this.state = { cartas:[] };
+  }
+
+  componentDidMount(){
+    fetch('tarot.json')
+    .then(response => response.json())
+    .then(cartas =>{
+        this.setState({cartas: cartas.cards});
+    });
   }
 
   render() {
@@ -15,14 +21,15 @@ class App extends Component {
       <section id="wrapper">
           <ul>
             {
-              this.state.cartas.map(function(carta, i){
+              this.state.cartas.map(function(carta, i){ 
+                carta.image = 'https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/' + carta.image
                 return(
-                    <div className="card" key="i">{carta.image}
+                    <div className="card" key={i}>
                       <div className="face side1">
                         <img src="http://www.adrianparr.com/images/card_back.png" />
                       </div>
                       <div className="face side2">
-                          <img src="{carta.image}" />
+                          <img src={carta.image} />
                       </div>  
                     </div>    
                   );
