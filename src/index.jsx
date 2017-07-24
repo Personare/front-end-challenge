2ceace185e17f8a42ed02e19f78ca95f3d68406b
laismarinhor <laismarinhor@gmail.com>
2017-07-24 19:06:11 -0300

    Consumindo json com axios

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Router, Route, IndexRoute, Link, hashHistory, HashRouter } from 'react-router'
+import Cartas from './app.jsx'
 
+
+ReactDom.render(
+    <Cartas />,
+	document.getElementById('taro')
+)
