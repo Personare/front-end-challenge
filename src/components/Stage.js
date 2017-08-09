455918fe8f900ab2c817c13bfc5f0b311e1e9769
Guilherme Louro <guilherme-louro@hotmail.com>
2017-08-08 21:57:55 -0300

    Criado testes para integração de Stage com Cards

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Menu } from 'components/Menu';
 import { Card } from 'components/Card';
 
@@ -6,9 +6,23 @@ export const Stage = (props) => (
     <div className="stage">
         <Menu />
         <div className="cards-list">
-            {props.order.map(item =>
-                <Card { ...item} />
+            {props.order.map(id =>
+                <Card
+                    key={id}
+                    name={props.cards[id].name}
+                    frontImg={props.cards[id].image}
+                    backImg={props.cards[id].imageBackCard}
+                />
             )}
         </div>
     </div>
 );
+
+Stage.propTypes = {
+    order: PropTypes.array.isRequired,
+    cards: PropTypes.shape({
+        name: PropTypes.string,
+        frontImg: PropTypes.string,
+        backImg: PropTypes.string,
+    }),
+};
--- tests/specs/components/Stage.spec.js
+++ tests/specs/components/Stage.spec.js
@@ -7,11 +7,18 @@ import { Card } from 'components/Card';
 
 describe('Stage - <Stage />', () => {
     let wrapper;
+    let cards;
 
     beforeEach(() => {
+        cards = {
+            1: { name: 'Lorem', image: 'http://lorem.com/1.jpg', imageBackCard: 'http://back.com/0.jpg' },
+            2: { name: 'Lorem', image: 'http://lorem.com/2.jpg', imageBackCard: 'http://back.com/0.jpg' },
+        };
+
         wrapper = shallow(
             <Stage
-                order={[1, 2]}
+                order={Object.keys(cards)}
+                cards={cards}
             />
         );
     });
@@ -24,9 +31,20 @@ describe('Stage - <Stage />', () => {
         expect(wrapper.find('.cards-list')).to.have.length(1);
     });
 
-    it('Deve a existir a quantidade de cards existentes na lista da props order', () => {
-        expect(wrapper.find(Card)).to.have.length(2);
+    context('Integração com Card:', () => {
+        it('Deve a existir a quantidade de cards existentes na lista da props order', () => {
+            expect(wrapper.find(Card)).to.have.length(2);
+        });
+
+        it('Deve a passar os items da props cards para o component Card', () => {
+            for (let i = 0; i < wrapper.find(Card).length; i++) {
+                expect(wrapper.find(Card).at(i).props().name).to.equal(cards[i + 1].name);
+                expect(wrapper.find(Card).at(i).props().frontImg).to.equal(cards[i + 1].image);
+                expect(wrapper.find(Card).at(i).props().backImg).to.equal(cards[i + 1].imageBackCard);
+            }
+        });
     });
+
 
     it('Deve existir o componente Menu', () => {
         expect(wrapper.find(Menu)).to.have.length(1);
