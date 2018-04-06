ea04f6b422d536ed71f5ee1f16def115acf2a2fb
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-06 18:48:54 -0300

    remove files unused

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render();
 registerServiceWorker();
