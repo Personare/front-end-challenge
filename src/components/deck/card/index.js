076a4b8b85dfc0a297611bca29bbe9d2347eb71d
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-07 19:38:47 -0300

    pass values to background using state. use <a> to represents the same mode of personare's tarot

--- src/components/deck/card/index.js
+++ src/components/deck/card/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
 
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
 
-const Card = ({ backImage, image }) => (
-  <figure className="card" />
-)
+    this.state = {
+      imageSrc: props.imageBackCard
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((prevState, props) => (
+      { imageSrc: (prevState.imageSrc === props.imageBackCard && props.image || props.imageBackCard) }
+    ));
+  }
+
+  render() {
+    const {name, image, imageBackCard} = this.props;
+    return (
+      <a className="card" name={name} onClick={this.handleClick} style={{backgroundImage: `url(${this.state.imageSrc})`}}/>
+    )
+  }
+}
 
 Card.propTypes = {
-  backImage: PropTypes.string,
+  imageBackCard: PropTypes.string,
   image: PropTypes.string,
+  name: PropTypes.string
 }
+
+export default Card;
