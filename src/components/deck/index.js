3de75b792eebe0719b385a3fd7aba3966e471ff3
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-07 19:37:38 -0300

    add deck component

---
+++ src/components/deck/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Card from './card';
+import { getCardInfos } from '../../api/cards';
+import './index.css';
+
+
+class Deck extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: []
+    };
+  }
+
+  componentDidMount() {
+    getCardInfos().then(infos => {
+      const { imageBackCard } = infos;
+      this.setState(
+        prevState => ({
+          cards: infos.cards.map((card, index) => {
+            const cardProps = { ...card, imageBackCard, image: `${infos.imagesUrl}${card.image}` };
+            return <Card {...cardProps} key={index} />;
+          })
+        })
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="deck">
+        {this.state.cards.map(card => card)}
+      </div>
+    );
+  }
+
+}
+
+export default Deck;
