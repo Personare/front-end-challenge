6c24adebd275aafe96ac7ba91d731c1f421313e5
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-08 16:03:24 -0300

    do shuffle only game starts and has not yet shuffled the cards

--- src/components/deck/index.js
+++ src/components/deck/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import Card from './card';
 import { getCardInfos } from '../../api/cards';
 import './index.css';
+import { GAME_STATUS } from '../game-context';
 
 
 class Deck extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: []
+      cards: [],
+      status: props.status,
+      isShuffling: false,
+      shuffled: false
+    };
+
+    this.shuffleCards = (cards = []) => {
+      this.isShuffling();
+      return cards
+        .map(card => [Math.random(), card])
+        .sort((card, nextCard) => card[0] - nextCard[0])
+        .map(card => card[1])
+    };
+
+    this.isShuffling = () => {
+      this.setState(prevState => ({ isShuffling: !prevState.isShuffling }));
+
+      setTimeout(() => {
+        this.setState(prevState => ({ isShuffling: !prevState.isShuffling,  shuffled: !prevState.shuffled }));
+      }, 2000);
     };
   }
 
@@ -16,25 +36,33 @@ class Deck extends React.Component {
     getCardInfos().then((infos) => {
       const { imageBackCard } = infos;
       this.setState({
-        cards: infos.cards.map((card, index) => {
-          const cardProps = { ...card, imageBackCard, image: `${infos.imagesUrl}${card.image}` };
-          return <Card {...cardProps} key={Symbol(index).toString()} />;
-        })
+        cards: infos.cards.map((card, index) => (
+          {
+            ...card,
+            imageBackCard,
+            image: `${infos.imagesUrl}${card.image}`
+          }
+        ))
       });
     });
   }
 
-  shuffleCards(cards = []) {
-    return cards
-      .map(card => [Math.random(), card])
-      .sort((card, nextCard) => card[0] - nextCard[0])
-      .map(card => card[1]);
+  componentDidUpdate(prevProps, prevStates, snapshot) {
+    if(this.props.status === GAME_STATUS.started && this.props.status !== prevProps.status && !this.state.shuffled) {
+      this.setState(prevState => ({
+        cards: this.shuffleCards(prevState.cards)
+      }));
+    }
   }
 
   render() {
     return (
-      <div className="deck">
-        {this.state.cards}
+      <div className={`deck ${this.state.isShuffling ? 'is-shuffling' : ''}`}>
+        {
+          this.state.cards.map((card, index) => (
+            <Card {...card} key={Symbol(index).toString()} />
+          ))
+        }
       </div>
     );
   }
