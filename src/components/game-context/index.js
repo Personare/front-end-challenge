6b549a8392a5a4d20e8a404a6dd2691cc610f7e1
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-07 21:58:01 -0300

    add game context

---
+++ src/components/game-context/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const GAME_STATUS = {
+  started: 'started',
+  off: 'off'
+};
+
+export const DEFAULT_VALUE = {
+  status: GAME_STATUS.off,
+  cardSelected: null,
+};
+
+export function startGame() {
+  this.setState({
+    status: GAME_STATUS.started
+  });
+}
+
+export function resetGame() {
+  this.setState({
+    status: GAME_STATUS.off
+  });
+}
+
+export const { Provider, Consumer } = React.createContext(DEFAULT_VALUE);
