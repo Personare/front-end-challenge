83ab48eae67dfddd5e4e4cc93b7fcf20a6c28a24
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-08 20:01:57 -0300

    show modal component when card is selected

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './index.css';
 import ButtonStart from '../buttons/start';
 import Deck from '../deck';
+import Modal from '../modal';
 import { Provider, DEFAULT_VALUE, startGame, resetGame, selectCard } from '../game-context';
+import CardImage from '../deck/card/image';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,10 +22,29 @@ class App extends React.Component {
   render() {
     return (
       <Provider value={this.state}>
-        <div className="app">
-          <Deck status={this.state.status}/>
-          <ButtonStart />
-        </div>
+        {
+          !this.state.cardSelected &&
+          <div className="app">
+            <Deck status={this.state.status}/>
+            <ButtonStart />
+          </div>
+        }
+        {
+          this.state.cardSelected &&
+          <Modal title={this.state.cardSelected.name}>
+            <div>
+              <CardImage {...this.state.cardSelected} />
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Ullam sint voluptas,
+              dignissimos beatae distinctio sed doloribus dolore
+              recusandae dolorum hic quo ipsam doloremque
+              fugit labore eligendi impedit
+              placeat odio odit!
+            </div>
+          </Modal>
+        }
       </Provider>
     );
   }
