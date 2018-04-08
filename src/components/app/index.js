bba21db04ab45f045d1067b6653608463d173863
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-07 21:58:31 -0300

    add state to use in provider

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,12 +3,29 @@ import React from 'react';
 import './index.css';
 import ButtonStart from '../buttons/start';
 import Deck from '../deck';
+import { Provider, DEFAULT_VALUE, startGame, resetGame } from '../game-context';
 
-const App = () => (
-  <div class="app">
-    <Deck />
-    <ButtonStart />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...DEFAULT_VALUE,
+      startGame: startGame.bind(this),
+      resetGame: resetGame.bind(this),
+    };
+  }
+
+  render() {
+    return (
+      <Provider value={this.state}>
+        <div className="app">
+          <Deck />
+          <ButtonStart />
+        </div>
+      </Provider>
+    );
+  }
+}
 
 export default App;
