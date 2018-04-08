16ab57af4804db16792b8dde4f732eeebc39e51f
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-08 16:05:16 -0300

    add default props

--- src/components/deck/card/image/index.js
+++ src/components/deck/card/image/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const CardImage = ({ image, modClass = '' }) => (
+const CardImage = ({ image, modClass = 'mod-front' }) => (
   <figure className={`card-image ${modClass}`}>
     <img src={image} />
   </figure>
 );
+
+CardImage.defaultProps = {
+  modClass: 'mod-front'
+};
 
 CardImage.propTypes = {
   image: PropTypes.string.isRequired,
