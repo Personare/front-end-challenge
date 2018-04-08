8c83f9e3a3ba568a7f1465e1ad293eadda4d5cb6
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-07 21:12:05 -0300

    add app component using deck and buttonstart.. (to-do: use react context... i want to test)

---
+++ src/components/app/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.css';
+import ButtonStart from '../buttons/start';
+import Deck from '../deck';
+
+const App = () => (
+  <div class="app">
+    <Deck />
+    <ButtonStart />
+  </div>
+);
+
+export default App;
