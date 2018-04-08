fd40776820843e7696d0da2aa4faac51b7bfc85a
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-08 16:04:34 -0300

    add flip status

--- src/components/deck/card/flip/index.js
+++ src/components/deck/card/flip/index.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 import CardImage from '../image';
 
-const CardFlip = ({ frontImage, backImage, flipped = false }) => (
-  <div className={`card-flip ${flipped ? 'mod-flipped' : ''}`}>
+const CardFlip = ({ frontImage, backImage, flipped = false }) => {
+  return <div className={`card-flip ${flipped ? 'mod-flipped' : ''}`}>
     <CardImage {...frontImage} modClass="mod-front" />
     <CardImage {...backImage} modClass="mod-back" />
   </div>
-);
+};
 
 CardFlip.defaultProps = {
   flipped: false
