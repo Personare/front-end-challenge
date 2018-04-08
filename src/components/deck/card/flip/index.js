7579193eb484f0d648228b13bebc9032122f36a2
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-08 00:25:15 -0300

    fix classname error

--- src/components/deck/card/flip/index.js
+++ src/components/deck/card/flip/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import CardImage from '../image';
 
 const CardFlip = ({ frontImage, backImage, flipped = false }) => (
-  <div className={`'card-flip ${flipped ? 'mod-flipped' : ''}'`}>
+  <div className={`card-flip ${flipped ? 'mod-flipped' : ''}`}>
     {frontImage && <CardImage {...frontImage} modClass={'mod-front'} />}
     {backImage && <CardImage {...backImage} modClass={'mod-back'}/>}
   </div>
