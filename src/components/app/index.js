a2c000743ef383ecdb4b28f304a154564b9588ac
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-08 22:03:29 -0300

    bind actions into app

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,19 +4,31 @@ import './index.css';
 import ButtonStart from '../buttons/start';
 import Deck from '../deck';
 import Modal from '../modal';
-import { Provider, DEFAULT_VALUE, startGame, resetGame, selectCard } from '../game-context';
+import { Provider, DEFAULT_VALUE, startGame, resetGame, selectCard, setInitialCards, isShuffling, GAME_STATUS } from '../game-context';
 import CardImage from '../deck/card/image';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       ...DEFAULT_VALUE,
       startGame: startGame.bind(this),
       resetGame: resetGame.bind(this),
-      selectCard: selectCard.bind(this)
+      selectCard: selectCard.bind(this),
     };
+
+    this.setInitialCards = setInitialCards.bind(this);
+    this.isShuffling = isShuffling.bind(this);
+  }
+
+  componentDidMount() {
+    this.setInitialCards();
+  }
+
+  componentDidUpdate(prevProps, prevStates) {
+    if(this.state.status === GAME_STATUS.off && prevStates.status !== this.state.status) {
+      this.setInitialCards();
+    }
   }
 
   render() {
@@ -25,7 +37,7 @@ class App extends React.Component {
         {
           !this.state.cardSelected &&
           <div className="app">
-            <Deck status={this.state.status}/>
+            <Deck />
             <ButtonStart />
           </div>
         }
