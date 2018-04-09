05c2ffb4e4b8251a3ef0af1d2208f49598a6e60b
Matheus Valeriano <math.valeriano@gmail.com>
2018-04-08 22:03:20 -0300

    remove all unnecessary code from deck

--- src/components/deck/index.js
+++ src/components/deck/index.js
@@ -2,68 +2,25 @@ import React from 'react';
 import Card from './card';
 import { getCardInfos } from '../../api/cards';
 import './index.css';
-import { GAME_STATUS } from '../game-context';
+import { GAME_STATUS, Consumer, DECK_STATUS } from '../game-context';
 
 
 class Deck extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: [],
-      status: props.status,
-      isShuffling: false,
-      shuffled: false
-    };
-
-    this.shuffleCards = (cards = []) => {
-      this.isShuffling();
-      return cards
-        .map(card => [Math.random(), card])
-        .sort((card, nextCard) => card[0] - nextCard[0])
-        .map(card => card[1])
-    };
-
-    this.isShuffling = () => {
-      this.setState(prevState => ({ isShuffling: !prevState.isShuffling }));
-
-      setTimeout(() => {
-        this.setState(prevState => ({ isShuffling: !prevState.isShuffling,  shuffled: !prevState.shuffled }));
-      }, 2000);
-    };
-  }
-
-  componentDidMount() {
-    getCardInfos().then((infos) => {
-      const { imageBackCard } = infos;
-      this.setState({
-        cards: infos.cards.map((card, index) => (
-          {
-            ...card,
-            imageBackCard,
-            image: `${infos.imagesUrl}${card.image}`
-          }
-        ))
-      });
-    });
-  }
-
-  componentDidUpdate(prevProps, prevStates, snapshot) {
-    if(this.props.status === GAME_STATUS.started && this.props.status !== prevProps.status && !this.state.shuffled) {
-      this.setState(prevState => ({
-        cards: this.shuffleCards(prevState.cards)
-      }));
-    }
-  }
-
   render() {
     return (
-      <div className={`deck ${this.state.isShuffling ? 'is-shuffling' : ''}`}>
+      <Consumer>
         {
-          this.state.cards.map((card, index) => (
-            <Card {...card} key={Symbol(index).toString()} />
-          ))
+          ({ status, deckStatus, cards }) => (
+            <div className={`deck ${deckStatus.isShuffling ? 'is-shuffling' : ''}`}>
+              {
+                cards.map((card, index) => (
+                  <Card {...card} key={Symbol(index).toString()} />
+                ))
+              }
+            </div>
+          )
         }
-      </div>
+      </Consumer>
     );
   }
 }
