1261a844b97dcbf6b3f5800c57e205ccbd46eca1
Guilherme Souza <zguilhermesouza@gmail.com>
2018-07-24 08:37:34 -0300

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    }
+}
