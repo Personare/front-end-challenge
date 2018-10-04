2c23fc2ae2049909689cf99552334dc113ecc63e
Diego T. Fialho <diego.tfialho@gmail.com>
2018-10-04 16:14:26 -0300

    Testes do Button component adicionados

---
+++ tests/specs/components/Button.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Button from '../../../src/components/Button/Button';
+
+describe('Button', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Button />);
+  });
+
+  it('deve renderizar corretamente', () => {
+    expect(wrapper.find('button')).toHaveLength(1);
+  });
+
+  it('deve renderizar com as classes corretamente', () => {
+    wrapper.setProps({ btnType: "Test" });
+
+    expect(wrapper.find('button').hasClass('Btn')).toBeTruthy();
+    expect(wrapper.find('button').hasClass('Test')).toBeTruthy();
+  });
+
+  it('deve renderizar o texto correto', () => {
+    wrapper.setProps({ children: 'Test Text' });
+
+    expect(wrapper.find('button').text()).toEqual('Test Text');
+  });
+});
