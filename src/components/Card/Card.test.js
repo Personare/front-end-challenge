d00d9a604abd658f6c3446006e1b5ff6dfb3b926
Bruno Braga <bruno.braga@ifactory.com.br>
2018-10-05 15:01:24 -0300

    Add enzyme library

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Card from './Card'
 import renderer from 'react-test-renderer'
+import Card from './Card'
 
 describe('Card component', () => {
     const props = {
