ec796fd584a4667e74337822f6cba9d38dad5b93
Diego T. Fialho <diego.tfialho@gmail.com>
2018-10-06 12:05:28 -0300

    Script para a aplicação ser servida em prod adicionado

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('dist'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
