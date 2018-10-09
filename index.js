2f6d8bf8845a6cf0aa58cd0b5373440bee422a5e
Diego T. Fialho <diego.tfialho@gmail.com>
2018-10-08 23:53:56 -0300

    Ajuste adicionado no script para rodar a aplicação em produção

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV.trim() === 'production') {
   app.use(express.static('dist'));
 
   app.get('*', (req, res) => {
