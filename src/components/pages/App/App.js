8599958f750c4608824029f72d17077921fbd901
Felipe César <jeanfelipecc@gmail.com>
2018-10-09 10:55:27 -0300

    Remove react logo

--- src/components/pages/App/App.js
+++ src/components/pages/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.scss';
 import Space from '../../atoms/Space/Space';
 import Cards from '../../organisms/Cards';
@@ -8,11 +7,10 @@ import MainHeader from '../../organisms/MainHeader';
 const App = () => (
     <div className="App">
         <Space />
-        <header className="App-header">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <main className="App-header">
             <MainHeader />
             <Cards />
-        </header>
+        </main>
     </div>
 );
 
