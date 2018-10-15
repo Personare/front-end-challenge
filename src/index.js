4b4bc6392bd103c01828e65bacc9d752ad132d67
Bruno Braga <bruno.braga@ifactory.com.br>
2018-10-15 20:31:18 -0300

    Remove unecessary code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { injectGlobal } from 'styled-components'
 import { Home } from './containers'
 
-render(
-  <Home />, document.getElementById('root')
-)
-
-
-injectGlobal`
-
-`
+render(<Home />, document.getElementById('root'))
