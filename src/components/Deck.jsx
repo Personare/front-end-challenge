69302fc6286721c2396fe965b32cc994a71bc24d
Eduardo Borges <eduardodmais.borges@gmail.com>
2018-10-22 19:58:43 -0300

    Adicionado mais alguns estados no Deck

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const Deck = ({ children, ...rest }) => {
-  return <div className="deck">{children}</div>;
+const Deck = ({
+  children,
+  isSpreaded = false,
+  isSorting = false,
+  isOpen = false,
+  isOverview = false,
+  ...rest
+}) => {
+  return (
+    <div
+      className={`deck
+      ${isOverview ? "is-overview" : ""}
+      ${isSorting ? "is-sorting" : ""}
+      ${isSpreaded ? "is-spreaded" : ""}
+      ${isOpen ? "is-open" : ""}`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Deck;
