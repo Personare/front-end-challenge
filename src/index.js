4b011a59ef38ad0e9ed8af21e4c0d35557eace1f
Eduardo Borges <eduardodmais.borges@gmail.com>
2018-10-22 14:25:51 -0300

    Estilos base

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import HotExported from "./exportHot";
 
+import "./styles/styles.sass";
+
 render(<HotExported />, document.getElementById("root"));
