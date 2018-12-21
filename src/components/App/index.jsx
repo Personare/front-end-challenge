b475c6ef8ee1fd80428eed36cb1a509570b0b867
Victor Freitas <victorfreitas1@gmail.com>
2018-12-21 11:11:26 -0200

    Start project

---
+++ src/components/App/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => (
+  <h1>
+    Hello
+  </h1>
+)
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+serviceWorker.unregister()
