07438fe308aa5d1147a237e3ccd86839d54abb86
Victor Freitas <victorfreitas1@gmail.com>
2018-12-22 01:48:25 -0200

    Add card info

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -4,6 +4,7 @@ import classNames from 'classnames'
 
 import styles from './Card.module.css'
 import CardImage from './CardImage'
+import CardInfo from './CardInfo'
 
 class Card extends Component {
   handleClick = () => {
@@ -36,15 +37,20 @@ class Card extends Component {
   }
 
   render() {
-    const { isShuffle, item } = this.props
+    const { isShuffle, item, currentCard: { name } } = this.props
 
     return (
-      <div {...this.getAttrs()}>
-        <CardImage
-          isShuffle={isShuffle}
-          item={item}
-          selected={this.isSelected()}
-        />
+      <div className={styles.content}>
+        <div {...this.getAttrs()}>
+          <CardImage
+            isShuffle={isShuffle}
+            item={item}
+            selected={this.isSelected()}
+          />
+        </div>
+        {this.isSelected()
+          ? <CardInfo name={name} />
+          : null}
       </div>
     )
   }
