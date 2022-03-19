6c0742b6d10734b23e648d144607b91fafd55dba
KlaussVP <klauss_mbc@hotmail.com>
2022-03-19 18:24:48 -0300

    feat: environment config - src directory created & application entry point

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { 
+  BrowserRouter as Router, 
+} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
