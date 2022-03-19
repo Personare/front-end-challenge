e37b498eb6157806cfc7504e167216c2f9eded0f
KlaussVP <klauss_mbc@hotmail.com>
2022-03-19 18:37:40 -0300

    feat: header component created

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HeaderContainer from "./styles";
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <h1>Tarô Personare</h1>
+    </HeaderContainer>
+  );
+}
