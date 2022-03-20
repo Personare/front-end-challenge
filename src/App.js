c6ecb0619683be955cd24bc5c7705ce5a504ff47
KlaussVP <klauss_mbc@hotmail.com>
2022-03-20 01:56:11 -0300

    feat: game screen initial layout

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Game from "./components/Game";
 
 export default function App() {
   return (
@@ -14,8 +15,8 @@ export default function App() {
       <Header />
       <Routes>
           <Route path="/" element={<Home />}/>
-          {/* <Route path="/game-on" element={<Game />}/>
-          <Route path="/end-game" element={<Result />}/> */}
+          <Route path="/game-on" element={<Game />}/>
+          {/* <Route path="/end-game" element={<Result />}/> */}
         </Routes>
     </Router>
   );
---
+++ src/components/Game/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "../Button";
+import { GameContainer, CardsContainer } from "./styles";
+
+export default function Game() {
+
+  return (
+    <GameContainer>
+      <Button />
+      <CardsContainer>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+        <img src="https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"/>
+      </CardsContainer>
+    </GameContainer>
+  );
+}
