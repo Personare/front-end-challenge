fad843a4ff0d58904d441c010630f4e9e9fbf77c
onesyss <o.y.mi@hotmail.com>
2022-03-28 14:31:24 -0300

    teste-tarot-personare

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './views/app/App';
+
+import './views/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
