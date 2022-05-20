eda694584c505b2366af24c79e32485dc9117dcf
Rodrigo Miranda <rodrigoalvesmiranda@yahoo.com.br>
2022-05-20 20:20:28 -0300

    chore: add typescript, eslint and prettier configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -66,7 +67,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
