8963afc0bd4c9962c079ad6804ef4aa5faff10f4
Andre <lasevo@gmail.com>
2022-07-24 19:28:41 -0300

    Componente Modal adicionado

---
+++ components/Modal/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles'
+
+type ModalProps={
+    onBackdropClick: () => void;
+    children: React.ReactNode;
+}
+
+const Modal = ({onBackdropClick, children}: ModalProps) =>(  
+  <S.ModalBackDrop onClick={onBackdropClick}>
+      <S.ModalContainer>
+        <S.Modal>
+          <S.ModalContent>
+            {children}
+          </S.ModalContent>
+        </S.Modal>
+    </S.ModalContainer>
+  </S.ModalBackDrop>
+    );
+
+
+export default Modal
