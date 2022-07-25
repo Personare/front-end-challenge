6007c5df04c34b5f6a9d3e2a1589f9736731eef6
Andre <lasevo@gmail.com>
2022-07-24 21:04:18 -0300

    Responsividade Modal component

--- components/Modal/styles.ts
+++ components/Modal/styles.ts
@@ -1,4 +1,5 @@
 import styled,{ keyframes } from "styled-components"
+import media from 'styled-media-query'
 
 const scaleIn = keyframes`
   0% {
@@ -13,14 +14,14 @@ export const Modal = styled.div`
   background-color: rgba(255, 255, 255, 0.35);
   backdrop-filter: blur(5px);
   padding: .7rem;
+  flex-direction: column;  
   border-radius: .5rem;
   display: flex;
-  flex-direction: column;
   align-items: center;
   position: relative;
   -webkit-animation: ${scaleIn} 700ms cubic-bezier(0.250, 0.460, 0.450, 0.940);
   animation: ${scaleIn} 700ms cubic-bezier(0.250, 0.460, 0.450, 0.940);
-`
+  `
 export const ModalBackDrop = styled.div`
   background-color: rgba($color: #000000c2, $alpha: 0.35);
   height: 100%;
@@ -33,6 +34,10 @@ export const ModalBackDrop = styled.div`
 `
 export const ModalContainer = styled.div`
   width: 60%;
+  ${media.lessThan('medium')`
+    max-height: 100%;
+    width: 90%;
+  `}
 `
 
 export const ModalContent = styled.div`
