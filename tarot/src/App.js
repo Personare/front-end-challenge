8a48acac7b139467b5a2e59534ba504ce353d2e1
FRANCINEIDETS <francineide_silva@hotmail.com>
2022-09-20 22:16:48 -0300

    inicio-do-case-bloco-1

---
+++ tarot/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import HomePage from "../src/pages/HomePage";
+//import SecundPage from "../src/pages/SecundPage";
+
+
+const App =()=> {
+
+  return (
+
+    <div>
+      <h1>tarot</h1>
+
+      {/* //<HomePage/>
+      //<SecundPage/> */}
+    </div>
+  )
+}
+
+export default App;
