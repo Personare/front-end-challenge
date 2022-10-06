0d7b5d8eac78773e5d809f7c61c7b3aa19a9c8fc
aloisio magalhaes <educacao1ouro@gmail.com>
2022-10-06 09:29:00 -0300

    vite config

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+       oi mundo
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
